9c0c4d454e456824186b2a83274ab0379344399a
pawel.niechoda <pawel.niechoda@relex.fi>
2017-10-22 21:31:16 +0300

    implement car validation

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,19 +1,61 @@
 module Lib
-    ( someFunc
-    ) where
+  (
+    listLength,
+    reverseList,
+    toDigitsRev,
+    toDigits,
+    doubleEveryOtherFromLeft,
+    doubleEveryOther,
+    sumDigits,
+    validate,
+    test,
+    mergeDigits
+  ) where
 
-someFunc :: IO ()
+listLength :: [Integer] -> Integer
+listLength [] = 0
+listLength (_:xs) = 1 + listLength xs
 
 reverseList :: [Integer] -> [Integer]
 reverseList [] = []
-reverseList (x:xs) = reverseList(xs) ++ [x]
+reverseList (x:xs) = reverseList xs ++ [x]
 
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev n
-    | n <= 0 = []
-    | otherwise =  (mod n 10) : toDigitsRev(div n 10)
+  | n <= 0 = []
+  | otherwise =  (mod n 10) : toDigitsRev(div n 10)
 
 toDigits :: Integer -> [Integer]
-toDigits n = reverseList(toDigitsRev(n))
+toDigits n = reverseList (toDigitsRev n)
 
-someFunc = print (toDigits (5069109))
+doubleEveryOtherFromLeft :: [Integer] -> [Integer]
+doubleEveryOtherFromLeft [] = []
+doubleEveryOtherFromLeft (x:xs)
+  | (mod (listLength xs) 2 ) == 0 = x * 2 : doubleEveryOtherFromLeft xs
+  | otherwise = x : doubleEveryOtherFromLeft xs
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther [] = []
+doubleEveryOther (x:xs)
+  | (mod (listLength xs) 2) == 1 = x * 2 : doubleEveryOther xs
+  | otherwise = x : doubleEveryOther xs
+
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits (x:xs) = x + sumDigits xs
+
+merge :: [Integer] -> [Integer] -> [Integer]
+merge [] ys = ys
+merge (x:xs) ys = x:merge ys xs
+
+mergeDigits :: [Integer] -> [Integer]
+mergeDigits [] = []
+mergeDigits (x:xs) = merge (toDigits x) (mergeDigits xs)
+
+test :: [Integer] -> Integer
+test n = sumDigits (mergeDigits (doubleEveryOther n))
+
+validate :: Integer -> Bool
+validate n
+  | rem (sumDigits (mergeDigits (doubleEveryOther (toDigits n)))) 10 == 0 = True
+  | otherwise = False
