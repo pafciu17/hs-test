a07ae7bf4b6c00873fab8566dac411a4c2952a5c
pawel.niechoda <pawel.niechoda@relex.fi>
2017-10-30 17:01:17 +0200

    add test case

--- test/HanoiSpec.hs
+++ test/HanoiSpec.hs
@@ -12,6 +12,8 @@ spec = do
     it "returns empty move list for 0 discs" $ do
       (hanoi 0 "a" "b" "c") `shouldBe` ([] :: [Move])
     it "should return list of correct moves" $ do
-      (hanoi 1 "a" "b" "c") `shouldBe` ([("a", "b")] :: [Move])
+      (hanoi 1 "a" "b" "c") `shouldBe` ([("a","b")] :: [Move])
       (hanoi 2 "a" "b" "c") `shouldBe` ([("a","c"), ("a","b"), ("c","b")] :: [Move])
       (hanoi 3 "a" "b" "c") `shouldBe` ([("a","b"), ("a","c"), ("b","c"), ("a", "b"), ("c", "a"), ("c", "b"), ("a", "b")] :: [Move])
+      (hanoi 4 "a" "b" "c") `shouldBe` ([("a","c"), ("a","b"), ("c","b"), ("a", "c"), ("b", "a"), ("b", "c"), ("a", "c"),
+        ("a", "b"), ("c", "b"), ("c", "a"), ("b", "a"), ("c", "b"), ("a", "c"), ("a", "b"), ("c", "b")] :: [Move])
